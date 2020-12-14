5df8a15be3eec844748f4e90e872102a0bd4c0c7
Alexis <alexysmcf@gmail.com>
2020-12-14 17:10:14 -0300

    exercises_14_12_20_17_10

--- hello.rb
+++ hello.rb
@@ -148,15 +148,120 @@
 # end
 # puts "THAU!!!"
 #
-puts "DIGITE UM ANO QUALQUER"
-x = gets.chomp.to_i
-puts "DIGITE UM ANO QUALQUER"
-y = gets.chomp.to_i
+# puts "DIGITE UM ANO QUALQUER"
+# x = gets.chomp.to_i
+# puts "DIGITE UM ANO QUALQUER"
+# y = gets.chomp.to_i
 
 
-while x<=y
-  if(x%4==0 or x%400==0)
-    puts x.to_s + " bissexto"
+# while x<=y
+#   if(x%4==0 or x%400==0)
+#     puts x.to_s + " bissexto"
+#   end
+#   x+=1
+# end
+#
+class Dragao
+  def initialize nome 
+    @nome = nome
+    @dormindo = false
+    @comidaEstomago =10 #ele está cheio
+    @comidaIntestino =0 #ele não precisa ir ao quintal
+
+    puts @nome + 'nasceu.'
   end
-  x+=1
+  def alimentar 
+    puts 'Você levou o ' +@nome+ '.'
+    @comidaEstomago = 10 
+    passagemDeTempo
+  end
+
+  def quintal
+    puts 'Você levou o' +@nome + "até o quintal."
+    @comidaIntestino = 0
+    passagemDeTempo
+  end
+  def colocarNaCama
+    puts 'Você colocou o ' + @nome + " na cama."
+    @dormindo = true
+    3.times do 
+      if @dormindo
+        passagemDeTempo
+      end
+      if @dormindo
+        @dormindo = false
+        puts @nome + "está acordando."
+      end
+    end
+
+    def jogar 
+      puts "Você joga o" +@nome+ " no ar."
+      puts "Ele dá uma risadinha e queima suas sobrancelhas."
+      passagemDeTempo
+    end
+    def balancar 
+      puts "Vocẽ balança o " +@nome + "gentilmente."
+      @dormindo = true
+      puts "Ele começa a cochilar..."
+      passagemDeTempo
+      if @dormindo
+        @dormindo = false
+        puts "...mas acorda quando você para."
+
+      end
+    end
+    private
+    #  "private" significa que os métodos definidos aqui
+    #  são métodos internos do objeto. (Você pode
+    #  alimentá-lo, mas você não pode perguntar se
+    #  ele está com fome.)
+    def comFome?
+      #  Nomes de métodos podem acabar com "?".
+      #  Normalmente, nós fazemos isso apenas
+      #  se o métodos retorna verdadeiro ou falso,
+      #  como esse:
+      @comidaEstomago <=2
+    end
+    def precisaSair?
+      @comidaIntestino >=8
+    end
+    def passagemDeTempo
+      if @comidaEstomago > 0
+        #Mover comida estômago para o intestino.
+        @comidaEstomago = @comidaEstomago -1
+        @comidaIntestino = @comidaIntestino +1
+      else #Nosso dragão está faminto!
+        if @dormindo
+          @dormindo = false
+          puts "Ele está acordando!"
+        end
+        puts @nome + "está faminto! Em desespero, ele comeu VOCÊ!"
+        exit #Isso sai do programa.
+      end
+      if @comidaIntetino>=10
+        @comidaIntestino =0
+        puts "Ele está acordando!"
+      end
+      puts "O estômado do "+@nome "está roncando..."
+    end
+    if precisaSair?
+      if @dormindo
+        @dormindo = false
+        puts "Ele está acordando!"
+      end
+      puts @nome + "faz a dança para ir ao quintal..."
+    end 
+  end
 end
+
+bichinho = Dragao.new "Norbert"
+bichinho.alimentar
+bichinho.jogar
+bichinho.quuintal
+bichinho.colocarNaCama
+bichinho.balancar
+bichinho.colocarNaCama
+bichinho.colocarNaCama
+bichinho.colocarNaCama
+bichinho.colocarNaCama
+
