45cdf197984d72496e48500cc62854663ff235f2
Alexis <alexysmcf@gmail.com>
2021-01-10 16:06:35 -0300

    exercises 10-01-2021_14-39 resolution the probleme Desafios Ruby / Analisando Shakespeare

--- read.rb
+++ read.rb
@@ -1,12 +1,20 @@
+#Chama a biblioteca 
 require 'nokogiri'
 require 'open-uri'
-@doc = Nokogiri::HTML(URI.open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
-  # all_speech = @doc.xpath("//speech + MALCOLM")
-all_speeches = @doc.xpath("//speech").remove.to_a
-  #puts all_speeches
-  a = all_speeches.map{ |s| { s.css("speaker/text()").to_s => s.css("line").count } }
 
-  cache = Hash.new { |h, k| h[k] = { k => 0 } }
+#cria-se uma variável de instancia e atribui-se o módulo Nokogiri responsável por chamar a url 
+@doc = Nokogiri::HTML(URI.open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
+
+# variável armazena  a variável de instância @doc xpath é uma propriedade de Nokogiri para localizar trechos de texto em html ou xml 
+# aqui as falas são retiradas do doc e salva como matriz
+all_speeches = @doc.xpath("//speech").remove.to_a
+
+#A partir da matriz de matrizes, fiz uma matriz de hashes para cada fala e suas linhas. Contagem
+a = all_speeches.map{ |s| { s.css("speaker/text()").to_s => s.css("line").count } }
+
+cache = Hash.new { |h, k| h[k] = { k => 0 } }
 puts a.flat_map(&:to_a).each_with_object(cache) { |(k,v),h| h[k][k] += v }.values
 
-  puts cache
+#Deleta o Hash "ALL" https://qastack.com.br/programming/10034678/how-can-i-delete-one-element-from-an-array-by-value
+cache.delete("ALL")
+puts cache
